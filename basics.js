338cd168d04330cd2beb98cd6584476221619e85
janellekwek <158569843+janellekwek@users.noreply.github.com>
2024-07-10 09:10:09 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,25 @@
+let myName = "Python";
+myName = "JS"; 
+const myAge = 18; // constant, cannot be changed
+let myPhone = 1234567
+myPhone = 342434242;
+let myArr = [1,2,3,4];
+let isHuman = true;
 
+console.log(myName);
+console.log(myAge);
+console.log(myPhone);
+console.log(myArr);
+console.log(isHuman);
+
+ // Playing with numbers
+ let a = (5 - 4) * 9;
+ let b = 5 % 2;
+ let c = 8 ** 2;
+ console.log(a, b, c);
+ 
+ let e = Math.random();
+ let f = Math.round(123.4545);
+ let g = Math.cos(Math.PI);
+ let h = Math.E ** 2;
+ console.log(e, f, g, h);
